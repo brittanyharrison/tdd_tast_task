8622d2b326328b58015716c39fe70f5515f4d838
Brittany Harrison <bkharrison94@gmail.com>
2021-06-28 14:48:24 +0100

    positive test added

--- modulus_test.py
+++ modulus_test.py
@@ -8,3 +8,6 @@ class Modulustest(unittest.TestCase):
 
     def test_modulus(self):
         self.assertEqual(self.mod.modulus(12, 3), 0)
+
+    def test_positive(self):
+        self.assertEqual(self.mod.positive(12, 3), True)
--- modulus_zero.py
+++ modulus_zero.py
@@ -2,3 +2,11 @@ class ModulusZero:
 
     def modulus(self,num1, num2):
         return num1 % num2
+
+    def positive(self, num1, num2):
+        if num1 < 0 :
+            return False
+        elif num2 < 0:
+            return False
+        else:
+            return True
