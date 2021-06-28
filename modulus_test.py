c1f65a2076c8524b2bf2f62455222e8a9b0ae9a7
Brittany Harrison <bkharrison94@gmail.com>
2021-06-28 15:03:06 +0100

    divisable test and method added

--- modulus_test.py
+++ modulus_test.py
@@ -6,8 +6,11 @@ from modulus_zero import ModulusZero
 class Modulustest(unittest.TestCase):
     mod = ModulusZero()
 
+    def test_divisable(self):
+        self.assertEqual(self.mod.divisable(12, 3), True)
+
     def test_modulus(self):
-        self.assertEqual(self.mod.modulus(12, 3), 0)
+        self.assertEqual(self.mod.modulus(11, 3), 2)
 
     def test_positive(self):
-        self.assertEqual(self.mod.positive(12, 3), True)
+        self.assertEqual(self.mod.positive(12, 2), True)
--- modulus_zero.py
+++ modulus_zero.py
@@ -1,12 +1,10 @@
 class ModulusZero:
 
-    def modulus(self,num1, num2):
+    def divisable(self, num1, num2):
+        return True if num1 % num2 == 0 else False
+
+    def modulus(self, num1, num2):
         return num1 % num2
 
     def positive(self, num1, num2):
-        if num1 < 0 :
-            return False
-        elif num2 < 0:
-            return False
-        else:
-            return True
+        return True if num1 >= 0 and num2 >= 0 else False
